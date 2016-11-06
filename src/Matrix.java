2619c60b0fd98c17071fa287f9dc33fe70e311dd
EugeneGavrilin <julentver1@gmail.com>
2016-11-06 18:07:46 +0300

    Matrix, multiplication realisation covered by one test

--- src/Matrix.java
+++ src/Matrix.java
@@ -11,11 +11,11 @@ final public class Matrix {
         rows = rowCount;
         columns = columnCount;
         matrix = new int[rowCount][columnCount];
-
     }
 
     public Matrix(int[][] matrix) {
-        // todo check
+        if (matrix == null)
+            matrix = new int[0][0];
 
         rows = matrix.length;
         columns = matrix[0].length;
@@ -29,15 +29,18 @@ final public class Matrix {
         this(other.matrix);
     }
 
-    private boolean checkInputMatrix(int[][] matrix){
-        return false;
+    private boolean checkInputMatrix() {
+        if (matrix == null)
+            return false;
+        else
+            return true;
     }
 
-    public int getElement(int row, int column){
+    public int getElement(int row, int column) {
         return matrix[row][column];
     }
 
-    public void setElement(int row, int column, int element){
+    public void setElement(int row, int column, int element) {
         matrix[row][column] = element;
     }
 
@@ -45,20 +48,59 @@ final public class Matrix {
         if (other.rows != this.rows || other.columns != this.columns)
             throw new RuntimeException("Illegal matrix dimensions.");
 
-        Matrix ret = new Matrix(rows,columns);
+        Matrix ret = new Matrix(rows, columns);
         for (int row = 0; row < rows; row++)
             for (int column = 0; column < columns; column++)
-                ret.setElement(row,column, this.getElement(row,column) + other.getElement(row,column));
+                ret.setElement(row, column, this.getElement(row, column) + other.getElement(row, column));
         return ret;
     }
 
-    public boolean equals(Matrix other){
+    public Matrix multiplyMatrix(Matrix right) {
+        Matrix left = this;
+        if (!left.checkInputMatrix() || !right.checkInputMatrix()) {
+            throw new IllegalArgumentException();
+        }
+        if (left.columns != right.rows) {
+            throw new IllegalArgumentException();
+        }
+
+        Matrix ret = new Matrix(left.rows, right.columns);
+        for (int row = 0; row < left.rows; row++) {
+            for (int column = 0; column < right.columns; column++) {
+                ret.setElement(row, column, multiplyVector(left.getRow(row), right.getColumn(column)));
+            }
+        }
+        return ret;
+    }
+
+    private static int multiplyVector(int[] row, int[] column) {
+        int ret = 0;
+        for(int index = 0; index < row.length; index++)
+            ret += row[index]*column[index];
+        return ret;
+    }
+
+    private int[] getColumn(int column) {
+        int[] ret = new int[rows];
+        for (int row = 0; row < rows; row++) {
+            ret[row] = matrix[row][column];
+        }
+        return ret;
+    }
+
+    private int[] getRow(int rowNumber) {
+        return matrix[rowNumber];
+    }
+
+    public boolean equals(Matrix other) {
+        if (columns != other.columns || rows != other.rows) {
+            return false;
+        }
         for (int row = 0; row < rows; row++)
             for (int column = 0; column < columns; column++)
-                if(this.matrix[row][column] != matrix[row][column])
+                if (other.matrix[row][column] != matrix[row][column])
                     return false;
         return true;
     }
-
 
 }
--- tests/MatrixTest.java
+++ tests/MatrixTest.java
@@ -1,6 +1,16 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MatrixTest {
+    @Test
+    public void multiplyMatrix() throws Exception {
+        Matrix first = new Matrix(new int[][]{{1,2},{1,2}});
+        Matrix second = new Matrix(new int[][]{{1,3},{1,4}});
+        Matrix expected = new Matrix(new int[][]{{3,11},{3,11}});
+        assertTrue(expected.equals(first.multiplyMatrix(second)));
+    }
+
     @org.junit.Test
     public void add() throws Exception {
         Matrix first = new Matrix(new int[][]{{1,2},{1,2}});
